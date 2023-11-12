861b08be85ce922e58b2efce31e8fbcceba43ea6
Christian Friedrich Coors <me@ccoors.de>
2023-11-12 20:45:31 +0100

    fix: function prototype

--- src/animations.h
+++ src/animations.h
@@ -9,7 +9,7 @@ uint8_t linesAnimation(uint8_t fill, uint8_t on, uint8_t off);
 uint8_t rotateAnimation(uint8_t fill, uint8_t on, uint8_t off);
 uint8_t circlesAnimation(uint8_t on, uint8_t off);
 void matrixAnimation();
-void sweepAnimation();
+void sweepAnimation(uint8_t on, uint8_t off);
 void waveAnimation();
 
 #endif  // ANIMATIONS_H_
